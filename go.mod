43ee3e845ac1769da93d14c50885767a2a33d282
Jyotiprokash Banerjee <jytib652@gmail.com>
2024-02-01 10:54:15 +0530

    modified routes.go

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module TopSongTracks
 
 go 1.21.4
 
-require github.com/go-chi/chi/v5 v5.0.11
+require (
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/go-chi/cors v1.2.1
+)
--- routes.go
+++ routes.go
@@ -5,13 +5,26 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func routes() http.Handler {
 	mux := chi.NewRouter()
 
+	// specify who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
 	mux.Use(middleware.Recoverer)
 	mux.Use(middleware.Logger)
+
 	mux.Get("/{countryName}", FavouriteArtistHandler)
 
 	return mux
